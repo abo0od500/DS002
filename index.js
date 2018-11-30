6acb9c49260a4aa4d07dea9845060d193fe8d2be
Abdullah <alkhamsi.abdullah@gmail.com>
2018-11-30 07:39:39 +0300

    Update
    Update

---
+++ index.js
@@ -0,0 +1,36 @@
+var discord = require("discord.js");
+var request = require('request');
+var Jimp = require('jimp');
+var inServer = 512728601041305600 ;
+var stringLength = require("string-length");
+
+var fs = require("fs");
+
+
+var cl = new discord.Client();
+
+
+    
+  
+
+cl.on("guildMemberAdd", (member) => {
+
+    // setTimeout(() => { 
+    var server = member.guild.id;
+    var servername = member.guild.name;
+    var d = new Date();
+    var user = cl.users.get(member.id); // USER MAP
+    
+    if(server != inServer){
+        
+
+        cl.users.get(user.id).send(`${process.env.MSG}`);
+
+
+   
+    }
+
+
+  });
+
+  cl.login(process.env.TOKEN);
